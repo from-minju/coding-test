55ccfd2ba8dbf753fe3391d7116fba9019ec9098
minju <deco1202@gmail.com>
2024-03-15 23:02:18 +0900

    240315 python study

--- PythonBaekjoon/sprout-test/python_study.py
+++ PythonBaekjoon/sprout-test/python_study.py
@@ -1,39 +1,49 @@
-#표준 모듈 - datetime
-import datetime
-print(datetime.date.today()) #2024-03-14
 
-#문자열 반복 연산자 *
-print("안녕하세요! "*3)
+#if문 예제
+tp = int(input("오늘의 온도는? : "))
+if tp>20:
+    print("따뜻하네용!")
+else:
+    print("추워요ㅜ")
 
-#문자선택연산자(인덱싱) []
-sentence = "초콜릿코스핫아이스"
-print(sentence[-4], sentence[0], sentence[3]) #핫 초 코
-print(sentence[-4]+sentence[0]+sentence[3]) #핫초코
+#bool타입
+if None: #False로 변환되는 값 : None, 0, 0.0, 빈 컨테이너
+    print("False")
+else:
+    print("True")
 
-#문자열범위선택연산자(슬라이싱) [a:b] ->[a]부터 [b-1]까지 출력됨. 
-print(sentence[-3:]+sentence[:3]) #아이스초콜릿
-print(sentence[3:5]) #코스
+#논리연산자 : and, or, not
+num1 = 15
+if(10<= num1 <=20): #python3에서는 두개의 비교식을 and로 조합하는방법 외에 이렇게도 가능
+    print(str(num1)+"은(는) 10이상 20이하입니다.")
 
-#문자열의 길이를 알아내는 함수 len()
-print(len(sentence))
+#if문예제 - 윤년을 판정하는 프로그램
+#윤년의 조건 : 
+#   1. 기본적으로 연도 수가 4로 나누어 떨어지면 윤년
+#   2. 그러나 100으로 나누어 떨어지는 해는 윤년이 아님. ->100으로 나누어 떨어지지 않으면 윤년
+#   3. 그러나 400으로 나누어 떨어지는 해는 윤년
 
-#input
-name = input("お名前は？") #사용자로부터 입력을 요구할때의 멘트를 같이 쓸 수 있음.
-print("こんにちは"+name+"さん")
-print(type(name)) #input함수는 무조건 str형태로 받아옴. 숫자도 str으로 받음.
+year = int(input("서기 몇 년? : "))
+result = True
 
+#방법1
+if (year%4==0 and year%100!=0) or year%400==0:
+    result = True
+else:
+    result = False
 
-height = input("당신의 키를 인치로 바꿔드립니다.\n당신의 키는 몇cm입니까? ->")
-inch = int(height)/2.54
-print(type(inch)) #float
+##방법2
+if year%4==0:
+    if year%100==0:
+        result = False
+        if year%400==0:
+            result = True
+else:
+    result = False
 
-inch = float(height)/2.54
-print("-->당신의 키는 {0}inch입니다.\n({cm}cm = {inch}inch)"
-      .format(round(inch,2), cm=height, inch=inch))
+#결과 출력
+if result==True:
+    print("윤년입니다.")
+else:
+    print("윤년이 아닙니다.")
 
-#수치와 문자열을 변환하는 함수
-#연산할때 형을 반드시 맞춰야 함. ->숫자와 숫자, 문자와 문자
-print(str(height))
-# print(type( str(10) ))
-print(float(10))
-print(int(3.5))
