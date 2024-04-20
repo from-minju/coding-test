472bcb53b9b467965d3900dd430e8b584e7740c9
minju <deco1202@naver.com>
2024-04-21 02:49:25 +0900

    5-10.py

---
+++ PythonBaekjoon/sprout-test/algorithm/DFS, BFS/5-11.py
@@ -0,0 +1,14 @@
+# 실전문제: [미로 탈출]
+
+from collections import deque
+
+# N, M을 공백을 기준으로 구분하여 입력 받기
+n, m = map(int, input().split())
+# 2차원 리스트의 맵 정보 입력 받기
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
