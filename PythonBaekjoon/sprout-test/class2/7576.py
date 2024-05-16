d6246690397086dc796ade88d9d146983d62633f
minju <deco1202@naver.com>
2024-05-17 02:34:55 +0900

    [Baekjoon] 7576. 토마토

--- PythonBaekjoon/sprout-test/class2/7576.py
+++ PythonBaekjoon/sprout-test/class2/7576.py
@@ -2,53 +2,50 @@
 
 
 from collections import deque
-graph = []
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 m,n = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
+q = deque()
+
+# 큐에 맨 처음 입력받은 토마토의 위치 좌표를 append()
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            q.append((i,j))
 
 
-def bfs(a, b):
-    q = deque()
-    depth = 0
-    q.append((a,b,depth))
-    graph[a][b] = 2
+def bfs():
 
     while q:
-        x, y, depth = q.popleft()
+        x, y = q.popleft()
 
         for i in range(4):
             tx = x + dx[i]
             ty = y + dy[i]
 
-            if 0<= tx <= n-1 and 0<= ty <= m-1 and graph[tx][ty]==0:
-                q.append((tx,ty,depth + 1))
-                graph[tx][ty] = 2
-    
-    return depth
+            if 0<= tx < n and 0<= ty < m and graph[tx][ty]==0:
+                q.append((tx,ty))
+                # 일 수를 좌표값에 저장한다.
+                graph[tx][ty] = graph[x][y] + 1
 
+# 토마토 익히기
+bfs()
 
-days = 0
+# 정답이 담길 변수
+result = 0
 
-for i in range(n):
-    for j in range(m):
-
-        if graph[i][j] == 1:
-            tdays = bfs(i,j)
-            days = max(tdays, days)
-
-
+# 아직도 익지 않은 토마토가 있는지 탐색
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 0:
-            days = -1
-            break
-    
-    if days == -1: break
+            print(-1)
+            exit(0)
+        # 최댓값이 정답이다.
+        result = max(result, graph[i][j])
 
-print(days)
+# 처음시작을 1일에서 시작해 +1해나갔으므로 -1해준다.
+print(result-1)
