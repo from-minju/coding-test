266101080a989a9075846c2c7fc039d918b842b4
minju <deco1202@naver.com>
2024-06-14 03:03:15 +0900

    [Goorm] 큰 수식 찾기

--- Goorm/level2/큰 수식 찾기.py
+++ Goorm/level2/큰 수식 찾기.py
@@ -1,27 +1,56 @@
-operator = ['*', '+', '-']
 
-operators = []
-numbers = []
 
-def separate_str (str) :
+# 연산자와 숫자로 나눠주는 함수
+def str_to_list (str) :
+    formula = []
 
     while True:
-
         for i in range(len(str)):
-            if str[i] in operator:
-                operators.append(str[i])
-                numbers.append(int(str[:i]))
+            if str[i] in ['*', '+', '-']:
+                formula.append(int(str[:i]))
+                formula.append(str[i])
                 str = str[i+1:]
                 break
         else:
-            numbers.append(int(str))
+            formula.append(int(str))
             break
-        
-
     
+    return formula
+
+# 계산한값을 리턴하는 함수
+def calculate (formula):
+    # * 계산
+    while True:
+        for i in range(len(formula)):
+            if formula[i] == '*':
+                temp = formula[i-1] * formula[i+1]
+                formula = formula[:i-1] + [temp] + formula[i+2:]
+                break
+        else:
+            break
+    
+    # +, - 계산
+    while True:
+        for i in range(len(formula)):
+            if formula[i] in ['+', '-']:
+                if formula[i] == '+':
+                    temp = formula[i-1] + formula[i+1]
+                elif formula[i] == '-':
+                    temp = formula[i-1] - formula[i+1]
+                formula = formula[:i-1] + [temp] + formula[i+2:]
+                break
+        else:
+            break
+    
+    return formula[0]
     
 
-string = input()
-separate_str(string)
-print(operators)
-print(numbers)
+
+A, B = input().split()
+A = calculate(str_to_list(A))
+B = calculate(str_to_list(B))
+
+if A > B:
+    print(A)
+else:
+    print(B)
