5bd4336fe36fc6474f98a4af2aa99ee70b4bb947
minju <deco1202@naver.com>
2024-07-22 10:59:37 +0900

    [Programmers] 햄버거 만들기.py

--- Programmers/level1/햄버거 만들기.py
+++ Programmers/level1/햄버거 만들기.py
@@ -2,6 +2,50 @@
 리턴: 포장가능한 햄버거 개수
 순서: 빵-야채-고기-빵 (1-2-3-1)
 '''
+
+# 실패 - 시간초과
 def solution(ingredient):
     answer = 0
+
+    stack = []
+    for n in ingredient:
+        stack.append(n)
+        if len(stack) >=4 and stack[-4:] == [1, 2, 3, 1]:
+            stack = stack[:-4]
+            answer += 1
+
+    return answer
+
+# 성공 
+def solution2(ingredient):
+    answer = 0
+
+    stack = []
+    for n in ingredient:
+        stack.append(n)
+        if len(stack) >=4 and stack[-4:] == [1, 2, 3, 1]:
+            stack.pop()
+            stack.pop()
+            stack.pop()
+            stack.pop()
+
+            answer += 1
+
+    return answer
+
+# 코드 개선
+def solution2(ingredient):
+    answer = 0
+
+    stack = []
+    for n in ingredient:
+        stack.append(n)
+        if stack[-4:] == [1, 2, 3, 1]:
+            stack.pop()
+            stack.pop()
+            stack.pop()
+            stack.pop()
+
+            answer += 1
+
     return answer
