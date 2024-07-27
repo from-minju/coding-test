a8ca8c47a8821a167cf0c180d504734f2f869876
minju <deco1202@naver.com>
2024-07-27 21:16:11 +0900

    [Programmers] 문자열 나누기

---
+++ Programmers/level1/문자열 나누기.py
@@ -0,0 +1,31 @@
+def solution(s):
+
+    result = 0
+    xCnt = 0 # x가 나온 횟수
+    yCnt = 0 # x가 아닌 다른 글자들이 나온 횟수
+    
+    while True:
+        
+        # 문자열의 남은 부분이 없으면 종료
+        if not s:
+            break
+        
+        x = s[0]
+        
+        for idx,i in enumerate(s):
+            if x == i:
+                xCnt += 1
+            else:
+                yCnt += 1
+            
+            if xCnt == yCnt:
+                result += 1
+                s = s[idx+1:]
+                break
+        # 두 횟수가 다른 상태에서 더 이상 읽을 글자가 없으면 종료
+        else:
+            result += 1
+            break
+        
+    return result 
+        
