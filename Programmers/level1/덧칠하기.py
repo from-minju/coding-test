95948c5f895e929bb6a8d7bdef7f7c4be0b8730f
minju <deco1202@naver.com>
2024-08-21 22:00:01 +0900

    [Programmers][Py] 다른버전풀이

--- Programmers/level1/덧칠하기.py
+++ Programmers/level1/덧칠하기.py
@@ -12,3 +12,28 @@ def solution(n, m, section):
             t = section[i] + m
     
     return cnt
+
+
+def solution0(n, m, section):
+    cnt = 0
+    t = 0
+    
+    for sec in section:
+        if sec < t:
+            continue
+        else:
+            cnt += 1
+            t = sec + m
+    
+    return cnt
+
+# 다른 풀이
+def solution2(n, m, section):
+    answer = 1
+    prev = section[0]
+    for sec in section:
+        if sec - prev >= m:
+            prev = sec
+            answer += 1
+
+    return answer
--- Programmers/level1/카드 뭉치.py
+++ Programmers/level1/카드 뭉치.py
@@ -20,4 +20,42 @@ def solution(cards1, cards2, goal):
     return "Yes"
             
 
+def solution1(cards1, cards2, goal):
+    cards1 = deque(cards1)
+    cards2 = deque(cards2)
+
+    for card in goal:
+        if cards1 and cards1[0] == card:
+            cards1.popleft()
+
+        elif cards2 and cards2[0] == card:
+            cards2.popleft()
+            
+        else:
+            return "No"
+        
+    return "Yes"
+
+
+def solution2(cards1, cards2, goal):
+    for card in goal:
+        if len(cards1) > 0 and card == cards1[0]:
+            cards1.pop(0)       
+        elif len(cards2) >0 and card == cards2[0]:
+            cards2.pop(0)
+        else:
+            return "No"
+    return "Yes"
+            
+
+def solution3(cards1, cards2, goal):
+    idx1,idx2=0,0
+    for word in goal:
+        if len(cards1)>idx1 and cards1[idx1]==word:
+            idx1+=1
+        elif len(cards2)>idx2 and cards2[idx2]==word:
+            idx2+=1
+        else:
+            return "No"
+    return "Yes"
         
