e249e8b4458e158125262b5ae76d5634810e3416
minju <deco1202@naver.com>
2024-08-27 23:47:30 +0900

    [Programmers][Py] lv1. 크기가 작은 부분문자열

--- Programmers/level1/크기가 작은 부분문자열.py
+++ Programmers/level1/크기가 작은 부분문자열.py
@@ -6,3 +6,15 @@ def solution(t, p):
     
     return cnt
 
+
+def solution(s):
+    answer = []
+    t = list(s)
+    for i in range(len(t)):
+        if t[i] in t[:i]:
+            answer.append(i-t.index(t[i]))
+            t[t.index(t[i])] = '0'
+        else:
+            answer.append(-1)
+    return answer
+
