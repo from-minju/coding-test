12620e9ee98b153e832bc9e44446e9a5799250c8
minju <deco1202@naver.com>
2024-10-07 20:15:28 +0900

    [Programmers/Py] lv2. 이모티콘 할인행사 ver2, 3

---
+++ Programmers/level2/이모티콘 할인행사_3.py
@@ -0,0 +1,46 @@
+from itertools import product
+
+def solution(users, emoticons):
+    # 모든 할인율의 조합
+    rates_data = list(product([10, 20, 30, 40], repeat = len(emoticons)))
+
+       
+    rates_data = []
+    fgoal1 = 0
+    fgoal2 = 0
+    
+    def dfs(arr, depth):
+        if depth == len(emoticons):
+            rates_data.append(arr[:])
+            return
+        for i in [10, 20, 30, 40]:
+            arr[depth] = i
+            dfs(arr, depth + 1)
+    
+    # 1. 모든 할인율 조합을 구한다. (이모티콘의 길이만큼)
+    dfs([0] * len(emoticons), 0)
+    
+    # 2. 할인율의 조합을 순회
+    for rates in rates_data:
+        goal1 = 0
+        goal2 = 0
+
+        for user in users:
+            totalpay = 0
+
+            for i in range(len(emoticons)):                            
+                if user[0] <= rates[i]:
+                    totalpay += emoticons[i] * (100 - rates[i]) / 100 
+                                        
+            if user[1] <= totalpay:
+                goal1 += 1
+            else:
+                goal2 += totalpay
+
+                                
+        if (fgoal1 < goal1) or (fgoal1 == goal1 and fgoal2 < goal2):
+            fgoal1 = goal1
+            fgoal2 = goal2
+
+    return [fgoal1, fgoal2]
+    
