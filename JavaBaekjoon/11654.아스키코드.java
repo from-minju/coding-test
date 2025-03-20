bd2579ae23b1b7918afe42a29fb99f4558fe8c06
jenny.kang <deco1202@naver.com>
2025-03-20 10:02:01 +0900

    [백준/자바] 11654. 아스키코드

---
+++ JavaBaekjoon/11654.아스키코드.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+
+// 11654. 아스키코드
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.nextLine().charAt(0);
+
+        int n = (int) c;
+
+        System.out.println(n);
+
+    }
+
+}
